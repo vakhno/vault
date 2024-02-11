029c682cd71484debefa33fa8cdbed72e9fb7e5a
Vakhno <vakhnohere@gmail.com>
2024-02-11 14:41:21 +0200

    Using use server directive

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,21 +1,45 @@
-import Image from "next/image";
+'use client';
+import { fetchAnime } from '@/app/action';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { AnimeProp } from '@/components/AnimeCard';
+import AnimeCard from '@/components/AnimeCard';
 
 function LoadMore() {
-  return (
-    <>
-      <section className="flex justify-center items-center w-full">
-        <div>
-          <Image
-            src="./spinner.svg"
-            alt="spinner"
-            width={56}
-            height={56}
-            className="object-contain"
-          />
-        </div>
-      </section>
-    </>
-  );
+	const { ref, inView } = useInView();
+	const [data, setData] = useState<AnimeProp[]>([]);
+	const [page, setPage] = useState(1);
+
+	useEffect(() => {
+		if (inView) {
+			fetchAnime(2).then((res) => {
+				setData([...data, ...res]);
+				setPage((prev) => ++prev);
+			});
+		}
+	}, [inView, data]);
+
+	return (
+		<>
+			<section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+				{data.map((item: AnimeProp, index: number) => (
+					<AnimeCard key={item.id} anime={item} index={index} />
+				))}
+			</section>
+			<section className="flex justify-center items-center w-full">
+				<div ref={ref}>
+					<Image
+						src="./spinner.svg"
+						alt="spinner"
+						width={56}
+						height={56}
+						className="object-contain"
+					/>
+				</div>
+			</section>
+		</>
+	);
 }
 
 export default LoadMore;
